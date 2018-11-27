17ce127faf65f3ebf4c116372b76438dc7038922
Melih Güler <melihguler999@gmail.com>
2018-11-27 09:15:13 +0300

    Db düzenleme yapıldı

--- webProje/webProje/Models/Haber.cs
+++ webProje/webProje/Models/Haber.cs
@@ -27,6 +27,7 @@ namespace webProje.Models
         public Nullable<System.DateTime> Tarih { get; set; }
         public Nullable<bool> Onay { get; set; }
         public string Baslik { get; set; }
+        public Nullable<int> OkunmaSayisi { get; set; }
     
         public virtual Admin Admin { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
