5355f16e00c278e868d5b16e17a03a26b5de0eb2
Melih Güler <melihguler999@gmail.com>
2018-12-06 13:06:34 +0300

    script

---
+++ WebProje/WebProje/Controllers/HaberYonlendirController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebProje.Models;
+
+namespace WebProje.Controllers
+{
+    public class HaberYonlendirController : Controller
+    {
+        // GET: HaberYonlendir
+        public ActionResult Index(Haber haber)
+        {
+            return View(haber);
+        }
+    }
+}
