8e34a29d0247e77cffd914a5dadba76192717535
Melih Güler <melihguler999@gmail.com>
2018-12-09 15:22:37 +0300

    sayacSorunsuzHaleGetirildi

--- WebProje/WebProje/Controllers/HaberYonlendirController.cs
+++ WebProje/WebProje/Controllers/HaberYonlendirController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebProje.DAL;
 using WebProje.Models;
 
 namespace WebProje.Controllers
@@ -10,8 +11,11 @@ namespace WebProje.Controllers
     public class HaberYonlendirController : Controller
     {
         // GET: HaberYonlendir
+       private VeriContext db = new VeriContext();
         public ActionResult Index(Haber haber)
         {
+            db.Haberler.Find(haber.HaberID).OkunmaSayisi += 1;
+            db.SaveChanges();
             return View(haber);
         }
     }
